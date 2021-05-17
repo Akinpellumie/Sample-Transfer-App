caf33cc4b86ac8fca89d4f1daaf28f4832b003e2
Provider Layi <akinlade3195@gmail.com>
2021-05-17 12:52:50 +0100

    editted

---
+++ app/src/main/java/com/interswitchng/interswitchpos/domain/models/SendCashTransferModel.kt
@@ -0,0 +1,9 @@
+package com.interswitchng.interswitchpos.domain.models
+
+public class SendCashTransfer {
+    val amount: String = ""
+    val acctNumber: String = ""
+    val acctName: String = ""
+    val bankName: String = ""
+    val narration: String = ""
+}
